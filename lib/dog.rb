7281631c362161c379f6c0669ac838ce6796ca2b
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-08-19 02:58:20 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    #attr_reader :name
+    attr_writer :name
+    #attr_accessor :name
 
+    #attr_reader :breed
+    attr_writer :breed
+    #attr_accessor :breed
+    def name(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def breed(breed)
+        @breed=breed
+    end
+
+    def breed 
+        @breed
+    end
 end
+
+# Fido = Dog.new
+# Fido.name='Fido'
+# Fido.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
 class Person
 
+    #attr_reader :name
+    attr_writer :name
+    #attr_accessor :name
+
+    #attr_reader :breed
+    attr_writer :job
+    #attr_accessor :job
+
+    def name(name)
+        @name=name
+    end
+
+    def name 
+        @name
+    end
+
+    def job(job)
+        @job=job
+    end
+
+    def job 
+        @job
+    end    
 end
+
+# Beyonce = Person.new
+# Beyonce.name=("Beyonce")
+# Beyonce.name
